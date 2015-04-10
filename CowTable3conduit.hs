29a6cf463ae805667bc2c807c204ba2dbfef0321
HirofumiSakurai <skrihrfm@plum.plala.or.jp>
2015-04-10 19:33:50 +0900

    Rails compatible version done.

--- CowTable3conduit.hs
+++ CowTable3conduit.hs
@@ -99,8 +99,10 @@ Kine json
 
 
 mkYesod "App" [parseRoutes|
-/ HomeR GET
+/        HomeR    GET
 /eSelect ESelectR GET
+/kine    KineR    GET
+/kine.json KineJsonR GET
 |]
 
 instance Yesod App
@@ -221,18 +223,85 @@ getESelectR = do
                   "t36" .= t36,"t37" .= t37,"t38" .= t38,"t39" .= t39,"t40" .= t40]
                 ,["t41" .= t41,"t42" .= t42,"t43" .= t43,"t44" .= t44,"t45" .= t45,
                   "t46" .= t46,"t47" .= t47,"t48" .= t48,"t49" .= t49,"t50" .= t50]]
-                -- "earNum".= earNum, "name" .= name, "birth" .= birth,
-                -- "sex" .= sex,  "ownerName" .= ownerName,
-                -- "t1"  .=  t1, "t2" .=  t2, "t3" .=  t3, "t4" .=  t4, "t5" .=  t5,
-                -- "t6"  .=  t6, "t7" .=  t7, "t8" .=  t8, "t9" .=  t9,"t10" .= t10,
-                -- "t11" .= t11,"t12" .= t12,"t13" .= t13,"t14" .= t14,"t15" .= t15,
-                -- "t16" .= t16,"t17" .= t17,"t18" .= t18,"t19" .= t19,"t20" .= t20,
-                -- "t21" .= t21,"t22" .= t22,"t23" .= t23,"t24" .= t24,"t25" .= t25,
-                -- "t26" .= t26,"t27" .= t27,"t28" .= t28,"t29" .= t29,"t30" .= t30,
-                -- "t31" .= t31,"t32" .= t32,"t33" .= t33,"t34" .= t34,"t35" .= t35,
-                -- "t36" .= t36,"t37" .= t37,"t38" .= t38,"t39" .= t39,"t40" .= t40,
-                -- "t41" .= t41,"t42" .= t42,"t43" .= t43,"t44" .= t44,"t45" .= t45,
-                -- "t46" .= t46,"t47" .= t47,"t48" .= t48,"t49" .= t49,"t50" .= t50]
+
+getKineR :: Handler Html
+getKineR = defaultLayout $ do
+  setTitle "Rails compatible version"
+  [whamlet|
+<h1> Rails compatible version
+<form action=@{KineJsonR} method="GET">
+    name: <input name="search" type="text" value="">
+    owner: <input name="search_owner" type="text" value="">
+    <input type="submit" value="送信">
+                 |]
+
+getKineJsonR :: Handler TypedContent
+getKineJsonR = do
+  name' <- runInputGet $ iopt textField "search"
+  owner' <- (runInputGet $ iopt textField "search_owner")
+  let name = nameConv name'
+      owner = (read $ T.unpack $ ownerConv owner') 
+  -- liftIO $ print flags
+  respondSourceDB typeJson $ cowsSrc name owner $= awaitForever toBuilder
+    where
+        nameConv (Just name) = name
+        nameConv (Nothing) = T.pack "%"
+        ownerConv (Just id) = id
+        ownerConv (Nothing) = T.pack "5"
+        cowsSrc name owner = 
+          E.selectSource
+            $ E.from $ \(kine) -> do
+                E.where_ $ (kine ^. KineOwnerId E.==. E.valkey owner)
+                           E.&&. ( kine ^. KineName `E.like` E.val name)
+                return $ (
+                  kine ^. KineEarNum, kine ^. KineName, kine ^. KineBirth,
+                  kine ^. KineSex, kine ^. KineOwnerId,
+                  (kine ^. KineT1, kine ^. KineT2, kine ^. KineT3, kine ^. KineT4,
+                   kine ^. KineT5, kine ^. KineT6, kine ^. KineT7, kine ^. KineT8,
+                   kine ^. KineT9, kine ^. KineT10),
+                  (kine ^. KineT11, kine ^. KineT12, kine ^. KineT13, kine ^. KineT14,
+                   kine ^. KineT15, kine ^. KineT16, kine ^. KineT17, kine ^. KineT18,
+                   kine ^. KineT19, kine ^. KineT20),
+                  (kine ^. KineT21, kine ^. KineT22, kine ^. KineT23, kine ^. KineT24,
+                   kine ^. KineT25, kine ^. KineT26, kine ^. KineT27, kine ^. KineT28,
+                   kine ^. KineT29, kine ^. KineT30),
+                  (kine ^. KineT31, kine ^. KineT32, kine ^. KineT33, kine ^. KineT34,
+                   kine ^. KineT35, kine ^. KineT36, kine ^. KineT37, kine ^. KineT38,
+                   kine ^. KineT39, kine ^. KineT40),
+                  (kine ^. KineT41, kine ^. KineT42, kine ^. KineT43, kine ^. KineT44,
+                   kine ^. KineT45, kine ^. KineT46, kine ^. KineT47, kine ^. KineT48,
+                   kine ^. KineT49, kine ^. KineT50))
+        toBuilder (E.Value earNum, E.Value name, E.Value birth,
+          E.Value sex, E.Value ownerId ,
+          (E.Value t1, E.Value  t2, E.Value  t3, E.Value  t4, E.Value  t5,
+          E.Value t6, E.Value  t7, E.Value  t8, E.Value  t9, E.Value t10),
+          (E.Value t11, E.Value t12, E.Value t13, E.Value t14, E.Value t15,
+          E.Value t16, E.Value t17, E.Value t18, E.Value t19, E.Value t20),
+          (E.Value t21, E.Value t22, E.Value t23, E.Value t24, E.Value t25,
+          E.Value t26, E.Value t27, E.Value t28, E.Value t29, E.Value t30),
+          (E.Value t31, E.Value t32, E.Value t33, E.Value t34, E.Value t35,
+          E.Value t36, E.Value t37, E.Value t38, E.Value t39, E.Value t40),
+          (E.Value t41, E.Value t42, E.Value t43, E.Value t44, E.Value t45,
+          E.Value t46, E.Value t47, E.Value t48, E.Value t49, E.Value t50))
+          = do 
+            sendChunkLBS $ encode $ object $ aesonList
+            where
+              aesonList = [
+                 "earNum".= earNum
+                ,"name" .= name
+                ,"birth" .= birth
+                ,"sex" .= sex
+                ,"ownerId" .= ownerId
+                ,"t1"  .=  t1, "t2" .=  t2, "t3" .=  t3, "t4" .=  t4,  "t5" .=  t5
+                 ,"t6"  .=  t6, "t7" .=  t7, "t8" .=  t8, "t9" .=  t9,"t10" .= t10
+                ,"t11" .= t11,"t12" .= t12,"t13" .= t13,"t14" .= t14,"t15" .= t15,
+                  "t16" .= t16,"t17" .= t17,"t18" .= t18,"t19" .= t19,"t20" .= t20
+                ,"t21" .= t21,"t22" .= t22,"t23" .= t23,"t24" .= t24,"t25" .= t25,
+                  "t26" .= t26,"t27" .= t27,"t28" .= t28,"t29" .= t29,"t30" .= t30
+                ,"t31" .= t31,"t32" .= t32,"t33" .= t33,"t34" .= t34,"t35" .= t35,
+                  "t36" .= t36,"t37" .= t37,"t38" .= t38,"t39" .= t39,"t40" .= t40
+                ,"t41" .= t41,"t42" .= t42,"t43" .= t43,"t44" .= t44,"t45" .= t45,
+                  "t46" .= t46,"t47" .= t47,"t48" .= t48,"t49" .= t49,"t50" .= t50]
 
 openConnectionCount :: Int
 openConnectionCount = 10
